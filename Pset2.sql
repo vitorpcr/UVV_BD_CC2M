98ea7d8925c7741e43dbcfe245e7a7d147cef497
Vitor Petindá Canêdo Ribeiro <100154869+vitorpcr@users.noreply.github.com>
2022-05-10 14:20:05 -0300

    Update Pset2.sql

--- Pset2.sql
+++ Pset2.sql
@@ -1,1 +1,21 @@
+SELECT AVG(salario) FROM funcionario
+ORDER BY departamento;
+
+SELECT AVG(salario) FROM funcionario
+WHERE sexo = H (AND sexo = M);
+
+SELECT AVG(salario) FROM funcionario
+WHERE sexo = M;
+
+SELECT primeiro_nome, nome_meio, ultimo_nome, idade, salario,
+CASE
+    WHEN salario < 35.000 THEN salario = 28.000
+    WHEN salario >= 35.000 THEN salario = 29.750
+END;
+
+SELECT nome_gerente, primeiro_nome,
+FROM funcionario
+ORDER BY (departamento ASC, salario DESC);
+
+
 
