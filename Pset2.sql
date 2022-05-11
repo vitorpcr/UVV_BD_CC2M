3e6c8901c36b9d112b8f41bb106cd2f9ba946c46
Vitor Petindá Canêdo Ribeiro <100154869+vitorpcr@users.noreply.github.com>
2022-05-11 14:34:32 -0300

    Update Pset2.sql

--- Pset2.sql
+++ Pset2.sql
@@ -17,6 +17,13 @@ SELECT nome_gerente, primeiro_nome,
 FROM funcionario
 ORDER BY (departamento ASC, salario DESC);
 
+SELECT primeiro_nome, nome_meio, ultimo_nome
+FROM funcionario
+WHERE nome_dependente IS NOT NULL AND 
+ORDER BY nome_dependente, idade, sexo 
+
 SELECT primeiro_nome, nome_meio, ultimo_nome, nome_departamento, salario,
 FROM funcionario
 WHERE nome_dependente IS NULL;
+
+
